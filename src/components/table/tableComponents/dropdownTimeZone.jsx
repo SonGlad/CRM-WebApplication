f0e76263f81ca32e2afdc95ecdadcc4ee5bc883a
artur <123599698+gritchin-artur@users.noreply.github.com>
2024-09-06 14:57:44 +0200

    table leads

--- src/components/table/tableComponents/dropdownTimeZone.jsx
+++ src/components/table/tableComponents/dropdownTimeZone.jsx
@@ -1,11 +1,30 @@
+import { useDispatch } from "react-redux";
 import { useLead } from "../../../hooks/useLead";
+import { patchTimeZone } from "../../../redux/Lead/lead-operation";
 
 
-export const DropdownTimeZone = ({ inputVisible, dropdownRef, handleDropdownItemClick }) => {
-
+export const DropdownTimeZone = ({ inputVisible, dropdownRef, leads, setLeads, setInputVisible}) => {
+const dispatch = useDispatch();
 
     const { timeZone: timeZoneData, isTimeZoneLoading } = useLead()
     const loading = "Loading...";
+
+        const handleDropdownItemClick = (e, leadIndex, zona, leadId) => {
+        e.preventDefault();
+          dispatch(patchTimeZone({ id: leadId, leadTimeZone: zona })).then(
+          (response) => {
+            if (response.payload.name) {
+              const updatedLeads = [...leads];
+              updatedLeads[leadIndex] = {
+                ...updatedLeads[leadIndex],
+                timeZone: zona,
+              };
+              setLeads(updatedLeads);
+              setInputVisible({ row: null, cell: null, leadId: null });
+            }
+          }
+        );
+  };
 
 
     return (
@@ -17,7 +36,7 @@ export const DropdownTimeZone = ({ inputVisible, dropdownRef, handleDropdownItem
                             className="ListItem"
                             key={item}
                             value={zona}
-                            onClick={() => handleDropdownItemClick(inputVisible.row, zona, "timeZone", inputVisible.leadId)}
+                            onClick={(e) => handleDropdownItemClick(e, inputVisible.row, zona, inputVisible.leadId)}
                         >
                             {zona}
                         </li>
