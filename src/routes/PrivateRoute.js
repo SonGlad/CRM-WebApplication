5cb2d5425eaf5e0bca9cc650c33909579ef6ce81
Oleg Koshevy <okoshevy@gmail.com>
2026-06-15 00:47:44 +0300

    test signed commit

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -7,5 +7,6 @@ export const PrivateRoute = ({component: Component, redirectTo = '/'}) => {
     const {isLoggedIn, isRefreshing} = useAuth();
     const shouldRedirect = !isLoggedIn && !isRefreshing;
 
+
     return shouldRedirect ? <Navigate to={redirectTo}/> : Component;
 };
